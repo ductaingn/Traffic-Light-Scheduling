d06788d631e7b8b5c382384dd5f22fc8c36d2d17
R4C00n <superc0605@gmail.com>
2024-08-18 16:43:17 +0700

    update

--- environment.py
+++ environment.py
@@ -1,18 +1,21 @@
 '''
-# This environment model provides a API for learning model to interact with SUMO simulation
+# This environment model provides a API for learning agent to interact with SUMO simulation
 
 To-do
 '''
+import torch
 
 class SUMOAPI:
     def __init__(self) -> None:
         pass
 
-    def perform_action(self, action):
-        pass
-
-    def get_state(self):
+    def get_next_state(self, args):
         return 
 
-    def get_reward(self):
+    def compute_reward(self, args):
         return
+    
+    def perform_action(self, action)->list[torch.Tensor, torch.Tensor]:
+        next_state = self.get_next_state()
+        reward = self.compute_reward()
+        return next_state, reward
--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 import torch
 import torch.multiprocessing as mp
+from torch.optim import Adam
 
 from model import ActorCriticModel
 from environment import SUMOAPI
 from utils.optimizer import SharedAdam
 
-def train_submodel(rank, num_state, num_action, critic_hidden_size, shared_model:torch.nn.Module, counter, lock, optimizer):
+MAX_COUNTER = 10000
+MAX_STEP = 1000
+
+def train_submodel(rank, num_state, num_action, critic_hidden_size, shared_model:torch.nn.Module, counter, lock, optimizer:SharedAdam, gamma):
     '''
     # Train a sub-model
 
@@ -17,20 +21,54 @@ def train_submodel(rank, num_state, num_action, critic_hidden_size, shared_model
     env = SUMOAPI()
 
     submodel = ActorCriticModel(num_state, num_action, shared_model, critic_hidden_size)
+    thread_step_counter = 1
 
     submodel.train()
 
-    state = env.get_state()
 
-    while True:
+    while counter <= MAX_COUNTER:
+        optimizer.zero_grad()
         submodel.load_state_dict(shared_model.state_dict())
+        t_start = thread_step_counter
+        state = env.get_state()
 
-        for step in range():
-            action, value_est = submodel(state)
+        rewards = []
+        values = []
+        action_probs = []
+        for step in range(t_start,MAX_STEP):
+            counter += 1
+            action_prob, value_est = submodel(state)
 
-            # To-do
+            dist = torch.distributions.Categorical(action_prob)
+            action = dist.sample()
 
+            next_state, reward = env.perform_action(action)
+
+            action_probs.append(action_prob)
+            values.append(value_est)
+            rewards.append(reward)
             
+            state = next_state
+
+        R = submodel.critic(state)
+
+        actor_loss, critic_loss = 0, 0
+        log_probs = torch.log(torch.Tensor(action_probs))
+
+        for i in reversed(range(len(rewards))):
+            R = reward[i] + gamma*R 
+            
+            advantage = reward[i] - values[i]
+            actor_loss += -log_probs[i]*(advantage)
+            critic_lost += (advantage)**2
+
+        total_loss = actor_loss + critic_loss
+        total_loss.backward()
+        
+        for submodel_param, shared_model_param in zip(submodel.parameters(), shared_model.parameters()):
+            shared_model._grad = submodel_param._grad
+
+        optimizer.step()
 
 def train():
     '''
@@ -57,7 +95,7 @@ def train():
     processes = []
 
     for rank in range(num_submodel):
-        p = mp.Process(target=train_submodel, args=(rank, shared_model, counter, lock, optimizer))
+        p = mp.Process(target=train_submodel, args=(rank, shared_model, counter, lock, optimizer,))
         p.start()
         processes.append(p)
 
